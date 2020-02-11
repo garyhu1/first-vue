629c35aa62fd5ad3171fd801d7392c364aa56eb9
huweiwei <huweiwei@superengine.com.cn>
2020-02-11 13:40:43 +0800

    测试mounted执行顺序

---
+++ src/views/MyTest.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>我只是一个测试</div>
+</template>
+
+<script>
+export default {
+    beforeMount() {
+        console.log("子beforeMount")
+    },
+    mounted() {
+        console.log("子mounted")
+    },
+    beforeCreate() {
+        console.log("子beforeCreate")
+    },
+    created() {
+        console.log("子created")
+    },
+}
+</script>
--- src/views/Study.vue
+++ src/views/Study.vue
@@ -1,5 +1,35 @@
 <template>
     <div>
         <h1>学习介绍</h1>
+        <MyTest></MyTest>
     </div>
 </template>
+
+<script>
+import MyTest from "./MyTest"
+export default {
+    components: { MyTest },
+    data() {
+        return {}
+    },
+    // template覆盖此处,此出不执行，删除template后执行此处
+    render() {
+        console.log("hellooooooo")
+        return (
+            <div>Hello Study</div>
+        );
+    },
+    beforeMount() {
+        console.log("父beforeMount")
+    },
+    mounted() {
+        console.log("父mounted")
+    },
+    beforeCreate() {
+        console.log("父beforeCreate")
+    },
+    created() {
+        console.log("父created")
+    },
+}
+</script>
